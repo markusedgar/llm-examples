2b477d43ccc0a481871597ece9b687fdd34c0405
markusedgar <markus@workplayexperience.com>
2024-06-24 09:03:19 +0200

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-import openai
+from openai import OpenAI
 
 # Everything is accessible via the st.secrets dict:
 # st.write("A cool secret:", st.secrets["OpenAI"]["openai_api_key"])
